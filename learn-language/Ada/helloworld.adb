852e614e023d4c7a3604b8e664ac600398935704
Akrom <akrommuhammadyusuf@gmail.com>
2026-07-19 12:28:27 +0700

    New: Learn programming language from helo World, Ada, C, C++, python and Rust

---
+++ learn-language/Ada/helloworld.adb
@@ -0,0 +1,6 @@
+with Ada.TEXT_IO; use Ada.Texxt_IO;
+
+procedur Hello is
+begin 
+    Put_Line("Hello, from Ada");
+end Hello;
---
+++ learn-language/C/helloworld.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello Wworld\n"); 
+    return 0;
+}
