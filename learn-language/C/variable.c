59745fc6f09abbc5504ed0671dfe5980ed1457ce
Akrom <akrommuhammadyusuf@gmail.com>
2026-07-19 13:27:54 +0700

    NewVariabel dalam C int dan char

---
+++ learn-language/C/variable.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a = 12;
+    int b = 6;
+    char name[] = "Akrom"; // pake [] agar bisa lebih dari 1 karakter 
+    int total_umur=  a + b;
+
+    printf("Hello my name is %s and im %dy old", name, total_umur);
+    return 0;
+}
